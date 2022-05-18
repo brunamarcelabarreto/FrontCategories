a000af6b70d9b7bf2dc79570e9e7ee530323060d
brunamarcelabarreto <bruna_marcelabarreto@hotmail.com>
2022-05-17 23:57:36 -0300

    First config

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Button>Home</Button>} />
+
+      <Route path="*" element= {<Navigate to="/home" />} />
+    </Routes>
+  );
+};
+
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material';
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      
+    }
+  }
+});
